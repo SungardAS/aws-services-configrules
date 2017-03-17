ae5179a53dd54d6e559aca8ba64e4719db0379f4
Alex Ough <alex.ough@sungardas.com>
2017-03-17 15:57:14 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,124 @@
+
+var AWS = require('aws-sdk');
+
+var baseHandler = require('aws-services-lib/lambda/base_handler.js')
+
+exports.handler = (event, context) => {
+  baseHandler.handler(event, context);
+}
+
+baseHandler.get = function(params, callback) {
+
+  var aws_config = new (require('aws-services-lib/aws/awsconfig.js'))();
+  var aws  = require("aws-sdk");
+
+  var input = {};
+  if (params.region) input['region'] = params.region;
+  if (params.credentials) {
+    input['creds'] = new AWS.Credentials({
+      accessKeyId: params.credentials.AccessKeyId,
+      secretAccessKey: params.credentials.SecretAccessKey,
+      sessionToken: params.credentials.SessionToken
+    });
+  }
+  console.log(input)
+
+  function succeeded(input) { callback(null, input.rules); }
+  function failed(input) { callback(null, {result: false}); }
+  function errored(err) { callback(err, null); }
+
+  var flows = [
+     {func:aws_config.getCreatedRules, success:succeeded, failure:failed, error:errored},
+  ];
+  aws_config.flows = flows;
+
+  flows[0].func(input);
+};
+
+baseHandler.post = function(params, callback) {
+
+  var aws_lambda = new (require('aws-services-lib/aws/lambda.js'))();
+  var aws_config = new (require('aws-services-lib/aws/awsconfig.js'))();
+  var aws  = require("aws-sdk");
+
+  function setCredentials(input) {
+    if (params.credentials) {
+      input['creds'] = new AWS.Credentials({
+        accessKeyId: params.credentials.AccessKeyId,
+        secretAccessKey: params.credentials.SecretAccessKey,
+        sessionToken: params.credentials.SessionToken
+      });
+      aws_lambda.enableRule(input);
+    }
+  }
+
+  function succeeded(input) { callback(null, {result: true}); }
+  function failed(input) { callback(null, {result: false}); }
+  function errored(err) { callback(err, null); }
+
+  var input = {
+     region: params.region,
+     ruleName: params.ruleName,
+     owner: params.owner,
+     sourceID: params.sourceID,
+     resourceType: params.resourceType,
+     descript: params.description,
+     params: params.params,
+  };
+  console.log(input);
+
+  if (params.owner == "CUSTOM_LAMBDA"){
+    input.messageType = params.messageType;
+    input.functionName = params.functionName;
+    input.principal = params.principal;
+    input.sourceAccount = params.customerAccount;
+    input.statementId = params.statementId; //unique string, some uuid from api
+    input.action = params.action;
+    var flows = [
+        {func:aws_lambda.addPermission, success:setCredentials, failure:failed, error:errored},
+        {func:setCredentials, success:aws_lambda.enableRule, failure:failed, error:errored},
+        {func:aws_config.enableRule, success:succeeded, failure:failed, error:errored},
+    ];
+  }
+  else{
+      var flows = [
+          {func:aws_config.enableRule, success:succeeded, failure:failed, error:errored},
+      ];
+  }
+
+  aws_config.flows = flows;
+  aws_lambda.flows = flows;
+
+  flows[0].func(input);
+};
+
+baseHandler.delete = function(params, callback) {
+
+  var aws_config = new (require('aws-services-lib/aws/awsconfig.js'))();
+  var aws  = require("aws-sdk");
+
+  function succeeded(input) { callback(null, {result: true}); }
+  function failed(input) { callback(null, {result: false}); }
+  function errored(err) { callback(err, null); }
+
+  var input = {
+      region: params.region,
+      ruleName: params.ruleName
+  };
+  if (params.credentials) {
+    input['creds'] = new AWS.Credentials({
+      accessKeyId: params.credentials.AccessKeyId,
+      secretAccessKey: params.credentials.SecretAccessKey,
+      sessionToken: params.credentials.SessionToken
+    });
+  }
+  console.log(input)
+
+  var flows = [
+      {func:aws_config.deleteRules, success:succeeded, failure:failed, error:errored},
+  ];
+
+  aws_config.flows = flows;
+
+  flows[0].func(input);
+};
---
+++ src/index_cloudwatchlogs.js
@@ -0,0 +1,64 @@
+
+var uuid = require('node-uuid');
+var aws_cloudwatchlog = new (require('aws-services-lib/aws/cloudwatchlog.js'))();
+
+var logGroupName = process.env.AWSCONFIG_RULE_LOG_GROUP_NAME;
+
+const createResponse = (statusCode, body) => {
+  return {
+    statusCode: statusCode,
+    body: body
+  }
+};
+
+exports.handler = function (event, context) {
+
+  console.log(event.Records[0].Sns);
+  var message_json = JSON.parse(event.Records[0].Sns.Message);
+  var region = event.Records[0].EventSubscriptionArn.split(":")[3];
+  var messageId = event.Records[0].Sns.MessageId;
+  var subject = event.Records[0].Sns.Subject;
+  var message = event.Records[0].Sns.Message;
+  var sentBy = event.Records[0].Sns.TopicArn;
+  var sentAt = event.Records[0].Sns.Timestamp;
+  var awsid = message_json.awsAccountId;
+  var non_complaint_pattern = /\s{1,}NON_COMPLIANT/;
+
+  if (non_complaint_pattern.test(subject)) {
+
+    function succeeded(input) { context.done(null, createResponse(200, true)); }
+    function failed(err) { context.fail(err, null); }
+    function errored(err) { context.fail(err, null); }
+
+    var logMessage = {
+      "awsid": awsid,
+      "subject": subject,
+      "message": message,
+      "sentBy": sentBy,
+      "sentAt": sentAt
+    };
+
+    var input = {
+      region: region,
+      groupName: logGroupName,
+      streamName: timestamp.replace(/:/g, '') + "-" + uuid.v4(),
+      logMessage: JSON.stringify(logMessage),
+      timestamp: (new Date()).getTime()
+    };
+    console.log(input);
+
+    var flows = [
+      {func:aws_cloudwatchlog.findLogGroup, success:aws_cloudwatchlog.findLogStream, failure:aws_cloudwatchlog.createLogGroup, error:errored},
+      {func:aws_cloudwatchlog.createLogGroup, success:aws_cloudwatchlog.findLogStream, failure:failed, error:errored},
+      {func:aws_cloudwatchlog.findLogStream, success:aws_cloudwatchlog.createLogEvents, failure:aws_cloudwatchlog.createLogStream, error:errored},
+      {func:aws_cloudwatchlog.createLogStream, success:aws_cloudwatchlog.createLogEvents, failure:failed, error:errored},
+      {func:aws_cloudwatchlog.createLogEvents, success:succeeded, failure:failed, error:errored}
+    ]
+    aws_cloudwatchlog.flows = flows;
+    flows[0].func(input);
+  }
+  else{
+    console.log("Non_Complaint_Alert_Message_False...Ignoring Alert Message.");
+    context.done(null, createResponse(200, true));
+  }
+}
